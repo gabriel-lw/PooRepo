49293e04ad94cd38abf09ee626929e3dd6c07c7b
gabriel-lw <gabriel.lw09aluno.ifsc.edu.br>
2026-09-15 20:16:40 -0300

    introdução à coleções

--- laboratorios/lab-03/exercicio-04/app/src/main/java/ads/poo/Data.java
+++ laboratorios/lab-03/exercicio-04/app/src/main/java/ads/poo/Data.java
@@ -13,6 +13,35 @@ public class Data {
 
     }
 
+    public Data(int dia){
+        
+        //janeiro ate 31
+        if(dia>0 && dia<=31){
+            this.dia = dia;
+        }else{
+            this.dia = 1;
+        }
+        //padroes
+        this.mes = 1;
+        this.ano = 1970;
+    }
+
+    public Data(int dia, int mes){
+
+        
+    }
+
+
+
+    public boolean ehBissexto(int ano){
+
+        if((ano %4 ==0) && (ano%100 ==0 && ano %400 ==0)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     
 
     
